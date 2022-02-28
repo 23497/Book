bca0f9ddfd5afc5c45c530e125f729c34345c86a
23497 <79152493+23497@users.noreply.github.com>
2022-02-27 19:41:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+function Next1(){
+    window.location = "page2.html";
+}
+
+function Back1(){
+    window.location = "index.html"
+}
+
+function Next2(){
+    window.location = "page3.html";
+}
+
+function Back2(){
+    window.location = "page2.html"
+}
+
+function Next3(){
+    window.location = "page4.html";
+}
+
+function Back3(){
+    window.location = "page3.html"
+}
+
+function Next4(){
+    window.location = "page5.html";
+}
+
+function Back4(){
+    window.location = "page4.html"
+}
+
+function Next5(){
+    window.location = "page6.html";
+}
+
+function Back5(){
+    window.location = "page5.html"
+}
+
+function Next6(){
+    window.location = "page7.html";
+}
+
+function Back6(){
+    window.location = "page6.html"
+}
+
+function Next7(){
+    window.location = "page8.html";
+}
+
+function Back7(){
+    window.location = "page7.html"
+}
+
+function Next8(){
+    window.location = "page9.html"
+}
